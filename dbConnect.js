92afee91cf2d10fd727b29b2b6ab3ba52dbdbd15
aarushtechnologies <aarushtechnologies7@gmail.com>
2025-08-27 23:14:34 +0530

    env file created

--- dbConnect.js
+++ dbConnect.js
@@ -1,7 +1,12 @@
-let mongoose = require('mongoose')
+// let mongoose = require('mongoose')
 
 // mongoose.connect('mongodb://127.0.0.1:27017/Codingwale')
 
-mongoose.connect('mongodb+srv://user1:dG57n2HLEuQx4NB9@cluster0.key0z.mongodb.net/lokseva?retryWrites=true&w=majority&appName=Cluster0')
+// console.log('Db Connected')
 
-console.log('Db Connected')
+let mongoose = require('mongoose');
+require('dotenv').config(); // Load .env file
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Db Connected'))
+  .catch(err => console.log(err));
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 let express = require('express')
 let app = express()
+
+const port = process.env.PORT || 4000;
+
+
+
 
 let RegisterRouter = require('./routes/registers.js')
 let OffersRouter = require('./routes/offers.js')
@@ -15,6 +21,6 @@ app.use('/api/birthdays/',BirthdaysRouter)
 app.use('/api/sales/',SalesRouter)
 app.use('/api/properties/',PropertiesRouter)
 
-app.listen(4000,()=>{
+app.listen(port,()=>{
     console.log('server started')
 })
